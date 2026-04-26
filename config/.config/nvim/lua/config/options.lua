73ba47983a9500ce85b04e6bd0b4da1552206f6e
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-04-26 22:32:46 +0200

    nvim: change style of options.lua

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -1,8 +1,16 @@
 -- SPDX-License-Identifier: GPL-3.0-only
 
+-- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Searching
+vim.opt.incsearch = true -- search as characters are entered
+vim.opt.hlsearch = false -- do not highlight matches
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+
+-- Others
 vim.opt.tabstop = 4 -- Display a tab as 4 spaces, not 8.
 vim.opt.softtabstop = 4 -- number of spaces in tab when editing
 vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
@@ -10,9 +18,3 @@ vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
 vim.opt.number = true -- show absolute line number
 vim.opt.relativenumber = true -- show relative line number
 vim.opt.termguicolors = true -- without this, nvim complains for some reason
-
--- Searching
-vim.opt.incsearch = true -- search as characters are entered
-vim.opt.hlsearch = false -- do not highlight matches
-vim.opt.ignorecase = true -- ignore case in searches by default
-vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
