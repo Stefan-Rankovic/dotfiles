e1e1ab69eb14545dfc98c7c3954ecc6edcdf09c9
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-04-27 00:02:36 +0200

    zsh: add documentation in the wal.zsh file

--- config/.config/zsh/functions/wal.zsh
+++ config/.config/zsh/functions/wal.zsh
@@ -3,22 +3,30 @@
 # [WAL]
 wal() {
 	echo -e "Custom wal behavior\n"
+
+	# Run wal with the arguments
 	command wal "$@"
 
+	# Search for -i argument
 	local wallpaper=""
 	local i_encountered=0
 	for arg in "$@"; do
+		# If there is an -i argument, store its value.
+		# For `wal -i image.png`, this stores `image.png`.
 		if [[ $i_encountered -eq 1 ]]; then
 			wallpaper=$(basename "$arg")
 			break
 		fi
 		[[ "$arg" == "-i" ]] && i_encountered=1
 	done
-	[[ $i_encountered -eq 0 ]] && return # Return if no -i argument.
+
+	# If no -i argument, return
+	[[ $i_encountered -eq 0 ]] && return 0
 
 	local pywal_dir="${PYWAL_CACHE_DIR:-${XDG_CACHE_HOME:?}/wal}"
 	local json_location="$pywal_dir/colors.json"
 
+	# Based on a certain wallpaper, change the wal colors if the default ones are undesirable.
 	case "$wallpaper" in
 		"hoshino_ai.png")
 			jq '.colors.color3 = "#9c4d08"' "$json_location" | sponge "$json_location" # Was #4A51A3
@@ -31,6 +39,8 @@ wal() {
 			;;
 	esac
 
-	wal --theme "$json_location"
+	# The earlier `case` statement changed the colors on one file.
+	# This applies the colors from that file to all other wal files.
+	command wal --theme "$json_location"
 }
 
