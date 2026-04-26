1aa8b19b4b6eaeeada1e2171f51bdaae181b346e
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-04-26 22:55:27 +0200

    nvim: remove useless {} wrap around neotree plugin

--- config/.config/nvim/lua/plugins/neo-tree.lua
+++ config/.config/nvim/lua/plugins/neo-tree.lua
@@ -5,19 +5,16 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim", -- https://github.com/MunifTanjim/nui.nvim
-			"nvim-tree/nvim-web-devicons", -- https://github.com/nvim-lua/plenary.nvim
-			{
-				"3rd/image.nvim", -- https://github.com/3rd/image.nvim
-				build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-				opts = { processor = "magick_cli" },
-			},
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim", -- https://github.com/MunifTanjim/nui.nvim
+		"nvim-tree/nvim-web-devicons", -- https://github.com/nvim-lua/plenary.nvim
+		{
+			"3rd/image.nvim", -- https://github.com/3rd/image.nvim
+			build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+			opts = { processor = "magick_cli" },
 		},
-		lazy = false, -- neo-tree will lazily load itself
 	},
+	lazy = false, -- It's already lazy
 }
