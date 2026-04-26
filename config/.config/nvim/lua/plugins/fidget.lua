62ca7a9ac75e1d88e1e7bf87579c62b2e77a42c1
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-04-26 22:37:09 +0200

    nvim: add plugin fidget that shows progress bars at the bottom right when, for example, rust-analyzer is loading (that behavior already existed somehow but this is the plugin for it; I have no idea what is going on)

---
+++ config/.config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,10 @@
+-- SPDX-License-Identifier: GPL-3.0-only
+--
+-- Shows some progress bars (?) at the bottom right.
+-- For example when rust-analyzer is doing its thing.
+--
+-- https://github.com/j-hui/fidget.nvim
+
+return {
+	"j-hui/fidget.nvim",
+}
